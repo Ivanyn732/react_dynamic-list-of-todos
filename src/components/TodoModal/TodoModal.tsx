2f7711b1dd4c2633e1a55b34cd4b84d7cf27ab7d
Ivanko <ivan.dovzhanyn@gmail.com>
2025-06-10 19:52:26 +0300

    Solution

--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,43 +1,60 @@
 import React from 'react';
-import { Loader } from '../Loader';
+import { Todo } from '../../types/Todo';
+import { User } from '../../types/User';
 
-export const TodoModal: React.FC = () => {
+type Props = {
+  selectedTodo: Todo | null;
+  user: User | null;
+  onClose: () => void;
+};
+
+export const TodoModal: React.FC<Props> = ({ selectedTodo, user, onClose }) => {
+  if (!selectedTodo || !user) {
+    return null;
+  }
+
   return (
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
-
-      {true ? (
-        <Loader />
-      ) : (
-        <div className="modal-card">
-          <header className="modal-card-head">
-            <div
-              className="modal-card-title has-text-weight-medium"
-              data-cy="modal-header"
-            >
-              Todo #2
-            </div>
-
-            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-            <button type="button" className="delete" data-cy="modal-close" />
-          </header>
-
-          <div className="modal-card-body">
-            <p className="block" data-cy="modal-title">
-              quis ut nam facilis et officia qui
-            </p>
-
-            <p className="block" data-cy="modal-user">
-              {/* <strong className="has-text-success">Done</strong> */}
-              <strong className="has-text-danger">Planned</strong>
-
-              {' by '}
-
-              <a href="mailto:Sincere@april.biz">Leanne Graham</a>
-            </p>
+      <div className="modal-card">
+        <header className="modal-card-head">
+          <div
+            className="modal-card-title has-text-weight-medium"
+            data-cy="modal-header"
+          >
+            Todo #{selectedTodo.id}
           </div>
+
+          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+          <button
+            type="button"
+            className="delete"
+            data-cy="modal-close"
+            onClick={onClose}
+          />
+        </header>
+
+        <div className="modal-card-body">
+          <p className="block" data-cy="modal-title">
+            {selectedTodo.title}
+          </p>
+
+          <p className="block" data-cy="modal-user">
+            {/* <strong className="has-text-success">Done</strong> */}
+            <strong
+              className={
+                selectedTodo.completed ? 'has-text-success' : 'has-text-danger'
+              }
+            >
+              {selectedTodo.completed ? 'Done' : 'Planned'}
+            </strong>
+
+            {' by '}
+
+            <a href={`mailto:${user.email}`}>{user.name}</a>
+          </p>
         </div>
-      )}
+      </div>
     </div>
   );
 };
